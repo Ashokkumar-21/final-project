ce9d585b9fd82339574f79b847be3b22c41a69f0
Ashokkumar-21 <116096679+Ashokkumar-21@users.noreply.github.com>
2025-08-15 14:09:15 +0530

    Update deploy.sh 2:09

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 IMAGE="$1"
 
 echo "Deploy $IMAGE"
-    docker pull "$IMAGE" &&
-    docker rm -f finalapp || true &&
-    docker run -itd --name finalapp -p 80:80 --restart unless-stopped '$IMAGE'
+    sudo docker pull "$IMAGE" &&
+    sudo docker rm -f finalapp || true &&
+    sudo docker run -itd --name finalapp -p 80:80 --restart unless-stopped '$IMAGE'
     
 echo "Final Project Deployment Done!"

1ec1b9244d131da2d096ad0de4f9bd5d86219f8c
Ashokkumar-21 <116096679+Ashokkumar-21@users.noreply.github.com>
2025-08-15 18:13:45 +0530

    Update deploy.sh 6:13

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,13 @@ set -euo pipefail
 
 IMAGE="$1"
 
-echo "Deploy $IMAGE"
-    sudo docker pull "$IMAGE" &&
-    sudo docker rm -f finalapp || true &&
-    sudo docker run -itd --name finalapp -p 80:80 --restart unless-stopped "$IMAGE"
+if [ -z "$IMAGE" ]; then
+  echo "Error: Docker image name is required!"
+  exit 1
+fi
 
+echo "Deploy $IMAGE"
+    sudo docker rm -f finalapp || true
+    sudo docker pull $IMAGE
+    sudo docker run -itd --name finalapp -p 80:80 --restart unless-stopped "$IMAGE"
 echo "Final Project Deployment Done!"

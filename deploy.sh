90e23f652b766d66a9a7f7fa3c0446eb36fdfae8
Ashokkumar-21 <116096679+Ashokkumar-21@users.noreply.github.com>
2025-08-15 13:40:06 +0530

    Update deploy.sh 1:40

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-DOCKERHUB_USER="ashok2102"
-DEV_REPO="dev"
-PROD_REPO="prod"
-BRANCH="${BRANCH}"
-TAG="${VERSION:-V1}"
+IMAGE="$1"
 
-APP_SERVER_USER="ubuntu"
-APP_SERVER_IP="65.2.177.203"
-
-echo "Chose Repo Based Commit"
-if [[ "$BRANCH" == "main" ]]; then
-  REPO="$PROD_REPO"
-else
-  REPO="$DEV_REPO"
-fi
-
-IMAGE="$DOCKERHUB_USER/$REPO:$TAG"
-
-echo "Deploy $IMAGE to $APP_SERVER_IP"
+echo "Deploy $IMAGE"
     docker pull '$IMAGE' &&
     docker rm -f finalapp || true &&
     docker run -itd --name finalapp -p 80:80 --restart unless-stopped '$IMAGE'
 "
 
-echo "Final Project Deployment Done! Visit: http://$APP_SERVER_IP/"
+echo "Final Project Deployment Done!"

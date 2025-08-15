dae1fb2b551e7449513298ee8d24c8f1e924d1a8
Ashokkumar-21 <116096679+Ashokkumar-21@users.noreply.github.com>
2025-08-15 13:55:20 +0530

    Update deploy.sh 1:55

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,5 @@ echo "Deploy $IMAGE"
     docker pull '$IMAGE' &&
     docker rm -f finalapp || true &&
     docker run -itd --name finalapp -p 80:80 --restart unless-stopped '$IMAGE'
-"
-
+    
 echo "Final Project Deployment Done!"

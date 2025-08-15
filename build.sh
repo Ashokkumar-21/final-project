0652d4a057378c7ce4ea1b389565e8f1429b5d0e
Ashokkumar-21 <116096679+Ashokkumar-21@users.noreply.github.com>
2025-08-15 18:13:08 +0530

    Update build.sh 6:13

--- build.sh
+++ build.sh
@@ -3,23 +3,25 @@
 set -e
 
 DOCKERHUB_USER="ashok2102"
-DEV_REPO="dev"
-PROD_REPO="prod"
-BRANCH="${BRANCH}"
-TAG="${VERSION:-v1}"
+BRANCH_NAME=$1
+TAG=$2
 
-if [[ "$BRANCH" == "main" ]]; then
-  REPO="$PROD_REPO"
+
+if [ "$BRANCH_NAME" == "main" ]; then
+  REPO_NAME="prod"
 else
-  REPO="$DEV_REPO"
+  REPO_NAME="dev"
 fi
 
-IMAGE="$DOCKERHUB_USER/$REPO:$TAG"
+IMAGE_NAME="${DOCKERHUB_USER}/${REPO_NAME}:${TAG}"
 
-echo "Building Image $IMAGE"
-docker build -t "$IMAGE" .
+echo "Building Docker image for $BRANCH_NAME"
+sudo docker build -t ${REPO_NAME}:$TAG .
 
-echo "Pushing Image to DockerHub $REPO"
-docker push "$IMAGE"
+echo "Tag $IMAGE_NAME"
+sudo docker tag ${REPO_NAME}:$TAG ${IMAGE_NAME}
 
-echo "Image Build & Push to DockerHub Done!"
+echo "Pushing Image to DockerHub $REPO_NAME"
+sudo docker push "$IMAGE_NAME"
+
+echo "$IMAGE_NAME Push to DockerHub Done!"

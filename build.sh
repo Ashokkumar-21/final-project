03933f2a9530ddb320f91549d4347087a53f2d9d
Ashokkumar-21 <116096679+Ashokkumar-21@users.noreply.github.com>
2025-08-15 18:19:45 +0530

    Update build.sh 6:19

--- build.sh
+++ build.sh
@@ -16,12 +16,12 @@ fi
 IMAGE_NAME="${DOCKERHUB_USER}/${REPO_NAME}:${TAG}"
 
 echo "Building Docker image for $BRANCH_NAME"
-sudo docker build -t ${REPO_NAME}:$TAG .
+docker build -t ${REPO_NAME}:$TAG .
 
 echo "Tag $IMAGE_NAME"
-sudo docker tag ${REPO_NAME}:$TAG ${IMAGE_NAME}
+docker tag ${REPO_NAME}:$TAG ${IMAGE_NAME}
 
 echo "Pushing Image to DockerHub $REPO_NAME"
-sudo docker push "$IMAGE_NAME"
+docker push "$IMAGE_NAME"
 
 echo "$IMAGE_NAME Push to DockerHub Done!"
